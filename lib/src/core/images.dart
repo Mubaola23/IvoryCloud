3607944db0fa3c09c9314b696353c12e73066946
unknown <toyyibat.abdulkareem@gmail.com>
2020-11-11 08:13:59 +0100

    splash screen, onboarding screen

--- lib/src/core/images.dart
+++ lib/src/core/images.dart
@@ -1,1 +1,9 @@
 // Define image paths here
+
+const logo = 'assets/app_logo.svg';
+const onboarding1 = 'assets/onboarding1.png';
+const onboarding2 = 'assets/onboarding2.png';
+const onboarding3 = 'assets/onboarding3.png';
+const home = 'assets/home.svg';
+const profile = 'assets/home.svg';
+const search = 'assets/search.svg';
